5c4f7276d8a72b4872ea08ae050ed560fd795076
Datkap <k.dzida@datkap.com>
2024-01-16 23:04:04 +0100

    full turtle race game completed

--- day_19/turtle_race.py
+++ day_19/turtle_race.py
@@ -3,14 +3,17 @@ import random
 
 colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange', 'pink', 'brown', 'gray', 'cyan']
 screen = Screen()
-height = 400
+height = 500
 width = 500
 screen.setup(width=width, height=height)
-number_of_turtles = int(screen.numinput(title="Number of turtles", prompt="How many turtles are going to participate in"
-                                                                      " the race?", minval=2, maxval=10))
+number_of_turtles = int(screen.numinput(title="Number of turtles", prompt="How many turtles are going to participate "
+                                                                          "in the race?", minval=2, maxval=10))
 playing_turtles_colors = random.sample(colors, number_of_turtles)
-user_bet = screen.textinput(title="Make your bet!", prompt="Which turtle will win the race? You can select"
-                                  f" from: {', '.join(playing_turtles_colors)}.")
+user_bet = ''
+while user_bet not in colors:
+    user_bet = screen.textinput(title="Make your bet!", prompt="Which turtle will win the race? You can select"
+                                                               f" from: {', '.join(playing_turtles_colors)}.")
+
 # Create playing turtles
 playing_turtles = {}
 for turtle_color in playing_turtles_colors:
@@ -21,20 +24,29 @@ for turtle_color in playing_turtles_colors:
 
 # Define game board parameters
 starting_x_position = (width / 2 - 20) * -1
-finishing_x_position = width / 2 - 10
+finishing_x_position = width / 2 - 25
 spacing = 40
 total_vertical_space = height - spacing * (len(playing_turtles) - 1)
 top_space = total_vertical_space / 2
-y_coordinates = [-(height / 2) + top_space + i * spacing for i in range(len(playing_turtles))]
-turtle_positions = {}
 
+for i in range(len(playing_turtles.keys())):
+    starting_y_position = -(height / 2) + top_space + i * spacing
+    playing_turtles[list(playing_turtles.keys())[i]].goto(x=starting_x_position, y=starting_y_position)
 
-# Move turtles to srtating position
-for turtle in playing_turtles.keys():
-    playing_turtles[turtle].goto(x=starting_x_position, y=y_coordinates[list(playing_turtles.keys()).index(turtle)])
-    turtle_positions[turtle] = playing_turtles[turtle].pos()
+turtle_won = False
 
-print(turtle_positions)
+while not turtle_won:
+    turtles_move_order = list(playing_turtles)
+    random.shuffle(turtles_move_order)
+    for turtle in playing_turtles:
+        moving_distance = random.randint(1, 10)
+        playing_turtles[turtle].forward(moving_distance)
+        if playing_turtles[turtle].xcor() >= finishing_x_position:
+            print(f"{turtle.capitalize()} won the race!")
+            if turtle == user_bet:
+                print("Your bet was correct!")
+            else:
+                print("Unfortunately you didn't guess it...")
+            turtle_won = True
 
 screen.exitonclick()
-
